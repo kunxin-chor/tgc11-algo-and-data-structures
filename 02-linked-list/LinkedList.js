0581159f8fb6a03a8588d0626b55c537a6cd6b6d
kunxin-chor <chorkunxin@yahoo.com>
2021-02-26 07:35:25 +0000

    Linked list demo

---
+++ 02-linked-list/LinkedList.js
@@ -0,0 +1,31 @@
+const Node = require('./Node');
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // add to back
+    addToBack(newNode) {
+        // case 1:
+        // if there is nothing in the list
+        // set head to the new node that we are adding
+        if (this.head == NULL) {
+            this.head = newNode
+        } else {
+            // case 2: if there is already something in the list
+            // find the last node and set its next to the new node
+
+            let current = this.head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            // current will be at the last node
+            current.setNext(newNode);
+        }
+
+     
+
+
+    }
+}
